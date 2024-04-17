34adeb5f9db1d5490db110ac2e60d66d4523f736
Milad Aghajohari <mi.aghajohari@gmail.com>
2024-04-16 20:30:20 -0400

    fix the logic of high contrast.

--- src/envs/eg_vectorized.py
+++ src/envs/eg_vectorized.py
@@ -295,7 +295,7 @@ class ObligatedRatioDiscreteEG(gym.Env):
             utilities_2 = torch.tensor([3, 10, 2]).repeat(self.batch_size, 1).to(self.device)
             item_pool = torch.tensor([4, 4, 4]).repeat(self.batch_size, 1).to(self.device)
         elif self.sampling_type == "high_contrast":
-            utilities_1 = torch.randint(0, 2, (self.batch_size, self.nb_items)).to(self.device) * self.utility_max
+            utilities_1 = torch.randint(0, 2, (self.batch_size, self.nb_items)).to(self.device) * (self.utility_max - 1) + 1
             utilities_2 = self.utility_max + 1 - utilities_1
             item_pool = torch.randint(1, self.item_max_quantity + 1, (self.batch_size, self.nb_items)).to(self.device)
         else:
