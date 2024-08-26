15ffbbab89db913c634e072bfe645e35647dbcfd
Juan Duque <juan.duque@mila.quebec>
2024-08-26 10:30:33 -0400

    Narval scripts changes.

--- sweep/sweep_narval_v1.py
+++ sweep/sweep_narval_v1.py
@@ -10,16 +10,17 @@ def gen_command(config):
 
 def run_random_job(fake_submit: bool = True):
     hparams = {
-        'optimizer_actor.lr': [1e-6, 3e-6, 7e-6, 3e-5, 1e-5, 3e-5, 7e-5, 1e-4, 3e-4],
-        'optimizer_critic.lr': [ 1e-6, 3e-6, 7e-6, 3e-5, 1e-5, 3e-5, 7e-5, 1e-4, 3e-4],
-        'training.entropy_beta': [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.3],
-        'training.clip_range': [0.1, 0.15, 0.2, 0.3, 0.4, 0.5],
+        'optimizer_actor.lr': [1e-6, 3e-6, 7e-6, 3e-5, 1e-5, 3e-5, 1e-4, 5e-4],
+        'optimizer_critic.lr': [ 1e-6, 3e-6, 7e-6, 3e-5, 1e-5, 3e-5, 1e-4, 5e-4],
+        'training.entropy_beta': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05],
+        'training.clip_range': [0.1, 0.15, 0.2, 0.25, 0.3, 0.4],
         'training.updates_per_batch': [1, 2, 3],
         'training.kl_threshold': [0.01, 0.05],
         'hidden_size': [128, 256, 512],
         'encoder.num_layers': [3, 4, 5, 6, 7],
         'training.critic_loss_mode': ['MC', 'td-1', 'interpolate'],
-        'max_cxt_len': [50, 100, 150],
+        'max_cxt_len': [30, 50, 70],
+        'optimizer_ss.lr': [1e-6, 3e-6, 7e-6, 3e-5, 1e-5, 3e-5],
     }
 
     # sample a random config
